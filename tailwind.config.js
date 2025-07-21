9c12bdcbda46ae5bac0fe0f6caede36b039944e6
Mohd Asad Ansari <asad.as9458@gmail.com>
2025-07-21 13:50:34 +0530

    taiwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  darkMode: 'class', // or 'media' if you want automatic based on OS
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
